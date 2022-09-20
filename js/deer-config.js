2a443040d5b262fc2dbe8ebffa3c9ef891c673c8
Patrick Cuba <cubap@slu.edu>
2022-09-20 14:33:30 -0500

    quick dump in

---
+++ js/deer-config.js
@@ -0,0 +1,78 @@
+import deerUtils from "./deer-utils.js"
+
+export default {
+    ID: "deer-id", // attribute, URI for resource to render
+    TYPE: "deer-type", // attribute, JSON-LD @type
+    TEMPLATE: "deer-template", // attribute, enum for custom template
+    KEY: "deer-key", // attribute, key to use for annotation
+    LABEL: "title", // attribute, alternate label for properties
+    CONTEXT: "deer-context", // attribute, JSON-LD @context, scoped
+    ATTRIBUTION: "deer-creator", // attribute, Web Annotation `creator`, scoped
+    MOTIVATION: "deer-motivation", // attribute, Web Annotation `motivation`, scoped
+    LIST: "deer-list", // attribute, property with resource array
+    COLLECTION: "deer-collection", // attribute, name of aggregating collection
+    LISTENING: "deer-listening", // attribute, name of container to watch for clicks
+    LINK: "deer-link", // attribute, location of href#[deer-id] for <a>s
+    VIEW: "deer-view, .deer-view", // selector, identifies render containers
+    FORM: "form[deer-type]", // selector, identifies data entry containers
+    ITEMTYPE: "deer-item-type", //attribute, specialty forms ('entity' by default)
+    SOURCE: "deer-source", // attribute, URI for asserting annotation
+    EVIDENCE: "nv-evidence", // attribute, URI for supporting evidence
+    INPUTTYPE: "deer-input-type", //attribute, defines whether this is an array list, array set, or object 
+    ARRAYDELIMETER: "deer-array-delimeter", //attribute, denotes delimeter to use for array.join()
+
+    INPUTS: ["input", "textarea", "dataset", "select"], // array of selectors, identifies inputs with .value
+    CONTAINERS: ["ItemList", "ItemListElement", "List", "Set", "list", "set", "@list", "@set"], // array of supported list and set types the app will dig into for array values
+    PRIMITIVES: [],
+
+    URLS: {
+        BASE_ID: "http://devstore.rerum.io/v1",
+        CREATE: "http://tinydev.rerum.io/app/create",
+        UPDATE: "http://tinydev.rerum.io/app/update",
+        QUERY: "http://tinydev.rerum.io/app/query",
+        OVERWRITE: "http://tinydev.rerum.io/app/overwrite",
+        SINCE: "http://devstore.rerum.io/v1/since"
+    },
+
+    EVENTS: {
+        CREATED: "deer-created",
+        UPDATED: "deer-updated",
+        LOADED: "deer-loaded",
+        NEW_VIEW: "deer-view",
+        NEW_FORM: "deer-form",
+        VIEW_RENDERED: "deer-view-rendered",
+        FORM_RENDERED: "deer-form-rendered",
+        CLICKED: "deer-clicked"
+    },
+
+    SUPPRESS: ["__rerum", "@context"], //properties to ignore
+    DELIMETERDEFAULT: ",", //Default delimeter for .split()ing and .join()ing 
+    ROBUSTFEEDBACK: true, //Show warnings along with errors in the web console.  Set to false to only see errors.  
+
+    /**
+     * Add any custom templates here through import or copy paste.
+     * Templates added here will overwrite the defaults in deer-render.js.
+     * 
+     * Each property must be lower-cased and return a template literal
+     * or an HTML String.
+     */
+    TEMPLATES: {
+        cat: (obj) => `<h5>${obj.name}</h5><img src="http://placekitten.com/300/150" style="width:100%;">`,
+        itemList: function (obj, options = {}) {
+            let tmpl = `<h2>Equipment</h2>`
+            if (options.list) {
+                tmpl += `<ul>`
+                obj[options.list].forEach((val, index) => {
+                    tmpl += `<li>
+                    <a href="${options.link}${val['@id']}">
+                    [ <deer-view deer-id="${val["@id"]}" deer-template="prop" deer-key="alternative"></deer-view> ] <deer-view deer-id="${val["@id"]}" deer-template="label">${index + 1}</deer-view>
+                    </a>
+                    </li>`
+                })
+                tmpl += `</ul>`
+            }
+            return tmpl
+        }
+    },
+    version: "alpha"
+}
